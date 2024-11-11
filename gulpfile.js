4011c456eb612f3dfb2e7e8ba5c814125371f615
Efraska <aux_sistemas@medicainsurgentes.com.mx>
2024-11-11 11:50:42 -0600

    Eliminar node_modules del repositorio

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+import { src, dest, watch } from 'gulp'
+import * as dartSass from 'sass'
+import gulpSass from 'gulp-sass'
+
+const sass = gulpSass(dartSass)
+
+export function css( done ) {
+    src('assets/scss/app.scss')
+        .pipe( sass() )
+        .pipe( dest('build/css') )
+
+    done()
+}
+
+export function dev() {
+    watch('assets/scss/app.scss', css)
+}
