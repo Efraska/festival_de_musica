c310b853d98f21ac46c9d8387d0fb43790057395
Efraska <aux_sistemas@medicainsurgentes.com.mx>
2024-11-14 16:36:37 -0600

    Comenzando con el Lineup y seccionde boletos

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,27 @@
-import { src, dest, watch } from 'gulp'
+import { src, dest, watch, series } from 'gulp'
 import * as dartSass from 'sass'
 import gulpSass from 'gulp-sass'
 
 const sass = gulpSass(dartSass)
 
+export function js( done ) {
+    src('assets/js/app.js')
+    .pipe( dest('build/js') )
+
+    done()
+}
+
 export function css( done ) {
-    src('assets/scss/app.scss')
+    src('assets/scss/app.scss', {sourcemaps: true})
         .pipe( sass().on('error', sass.logError) )
-        .pipe( dest('build/css') )
+        .pipe( dest('build/css', {sourcemaps: '.'}) )
 
     done()
 }
 
 export function dev() {
     watch('assets/scss/**/*.scss', css)
+    watch('assets/js/**/*.js', js)
 }
+
+export default series( js, css, dev )
